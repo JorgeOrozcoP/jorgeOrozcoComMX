34b43f31168dc17595e47085b71af541f3e7075c
Jorge Orozco <yorchorozco@gmail.com>
2016-05-14 14:31:22 -0600

    new redesinged working site! need to add more content

---
+++ config.js
@@ -0,0 +1,8 @@
+require.config({
+    paths: {
+    	"angular" : "/node_modules/angular/angular.min",
+        "jquery" : "/node_modules/jquery/dist/jquery.min",
+        "bootstrap" : "/node_modules/bootstrap/dist/js/bootstrap"  
+    },
+    	
+});
